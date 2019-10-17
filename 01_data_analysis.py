61315d331cfb05490dd4fb002bdc467f04e307e4
Alex Nim <alexnimjli@gmail.com>
2019-10-17 11:13:20 +0100

    simple data analysis

---
+++ 01_data_analysis.py
@@ -0,0 +1,114 @@
+# ---
+# jupyter:
+#   jupytext:
+#     formats: ipynb,py:light
+#     text_representation:
+#       extension: .py
+#       format_name: light
+#       format_version: '1.4'
+#       jupytext_version: 1.2.3
+#   kernelspec:
+#     display_name: Python 3
+#     language: python
+#     name: python3
+# ---
+
+# +
+import numpy as np
+import os
+
+np.random.seed(42)
+
+# %matplotlib inline
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+mpl.rc('axes', labelsize=14)
+mpl.rc('xtick', labelsize=12)
+mpl.rc('ytick', labelsize=12)
+
+# Ignore useless warnings
+import warnings
+warnings.filterwarnings(action="ignore", message="^internal gelsd")
+
+# +
+import pandas as pd
+pd.set_option('display.max_rows', 30)
+
+df = pd.read_csv("kag_risk_factors_cervical_cancer.csv")
+# -
+
+df.head()
+
+
+def missing_values_table(df):
+        # Total missing values
+        mis_val = df.isnull().sum()
+        
+        # Percentage of missing values
+        mis_val_percent = 100 * df.isnull().sum() / len(df)
+        
+        # Make a table with the results
+        mis_val_table = pd.concat([mis_val, mis_val_percent], axis=1)
+        
+        # Rename the columns
+        mis_val_table_ren_columns = mis_val_table.rename(
+        columns = {0 : 'Missing Values', 1 : '% of Total Values'})
+        
+        # Sort the table by percentage of missing descending
+        mis_val_table_ren_columns = mis_val_table_ren_columns[
+            mis_val_table_ren_columns.iloc[:,1] != 0].sort_values(
+        '% of Total Values', ascending=False).round(1)
+        
+        # Print some summary information
+        print ("Your selected dataframe has " + str(df.shape[1]) + " columns.\n"      
+            "There are " + str(mis_val_table_ren_columns.shape[0]) +
+              " columns that have missing values.")
+        
+        # Return the dataframe with missing information
+        return mis_val_table_ren_columns
+
+
+missing_values_table(df)
+
+df['Biopsy'].value_counts()
+
+df.groupby('Biopsy')['Hormonal Contraceptives'].value_counts()
+
+# +
+pd.options.display.max_rows = 4000
+
+df.columns
+
+
+# -
+
+def plot_bar_graphs(df, attribute, y):
+    plt.figure(1)
+    plt.subplot(131)
+    df[attribute].value_counts(normalize=True).plot.bar(figsize=(22,4),title= attribute)
+    
+    crosstab = pd.crosstab(df[attribute], df[y])
+    crosstab.div(crosstab.sum(1).astype(float), axis=0).plot.bar(stacked=True)
+    crosstab.plot.bar(stacked=True)
+    
+    res = df.groupby([attribute, y]).size().unstack()
+    tot_col = 0
+    for i in range(len(df[y].unique())):
+        tot_col = tot_col + res[res.columns[i]] 
+        
+    for i in range(len(df[y].unique())):    
+        res[i] = (res[res.columns[i]]/tot_col)
+    
+    res = res.sort_values(by = [0], ascending = True)
+    print(res)
+    
+    return
+
+
+plot_bar_graphs(df, 'First sexual intercourse', 'Biopsy')
+
+plot_bar_graphs(df, 'STDs: Number of diagnosis', 'Biopsy')
+
+plot_bar_graphs(df, 'STDs', 'Biopsy')
+
+
