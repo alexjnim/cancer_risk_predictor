81a7f4a46305a1b39ebfdf7ff83d33b45f60162c
Alex Nim <alexnimjli@gmail.com>
2019-10-17 16:03:25 +0100

    creating test files from the original data set

--- 02_preprocessing_data.py
+++ 02_preprocessing_data.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 # ---
 # jupyter:
 #   jupytext:
@@ -35,6 +36,51 @@ import pandas as pd
 pd.set_option('display.max_rows', 30)
 
 df = pd.read_csv("kag_risk_factors_cervical_cancer.csv")
+# -
+
+df = df.drop(['STDs: Time since first diagnosis', 'STDs: Time since last diagnosis'], axis=1)
+
+df.shape
+
+df.head()
+
+t = df[df != '?'] #this will change all ? into nan
+
+t.head()
+
+# seperate the dataframe into one with all rows that have nan values, and the other with no nan values
+nan_df = t[t.isna().any(axis=1)]
+nonan_df = t.dropna()
+
+nonan_df.head()
+
+# +
+from sklearn.model_selection import train_test_split
+
+# here we make a X_pretest set that has 2/10 of the rows that don't have nan values. that is 2/10 * 77271 = 15450, 
+# so 15% of the training dataset
+
+X_test, remaining_nonan = train_test_split(nonan_df, train_size= 0.2, random_state = 42)
+# -
+
+X_test.isnull().any(axis=0).sum()
+
+df = pd.concat([nan_df, remaining_nonan])
+
+df.shape
+
+
+def print_totshape(df1, df2):
+    m,n = df1.shape
+    a,b = df2.shape
+    
+    print(m+a)
+    return
+
+
+print_totshape(df, X_test)
+
+# # quick look at the correlation matrix
 
 # +
 import seaborn as sns
@@ -72,30 +118,17 @@ num_feats = _get_numerical_features(df, cat_feats)
 
 corr_matrix(df, num_feats, 'Biopsy')
 
-# # changing the '?' in all the categorical features into the median or delete and changing certain categorical features into numerical features
+# # filling the nan values and changing certain categorical features into numerical features
 
 cat_feats
 
 
-def delete_q(df, l):
-    for i in l:
-        df = df[df[i] != '?']
-        df[i].value_counts()
+def fill_mode(df, attribute_list):
+    for i in attribute_list:
         print(i)
-        print(df.shape)
+        df[i].fillna(df[i].mode()[0], inplace=True)
     return df
 
-
-def q_to_median(df, l):
-    for i in l:
-        df[i] = df[i].replace(['?'], [df[i].mode()])
-        df[i] = df[i].astype(float)
-        print(i)
-        print(df.shape)
-    return df
-
-
-df = df.drop(['STDs: Time since first diagnosis', 'STDs: Time since last diagnosis'], axis=1)
 
 l = ['Number of sexual partners',
      'First sexual intercourse',
@@ -123,13 +156,11 @@ l2 = [ 'Smokes',
  'STDs:Hepatitis B',
  'STDs:HPV',]
 
+df = fill_mode(df, l)
 
+df.shape
 
-
-
-df = delete_q(df, l2)
-
-df = q_to_median(df,l)
+df = df.dropna(how='any',axis=0) 
 
 cat_feats = _get_categorical_features(df)
 num_feats = _get_numerical_features(df, cat_feats)
@@ -140,13 +171,15 @@ num_feats
 
 corr_matrix(df, num_feats, 'Biopsy')
 
+# # creating X and y data
+
 X_train = df.drop('Biopsy', axis = 1)
 cat_feats = _get_categorical_features(X_train)
 num_feats = _get_numerical_features(X_train, cat_feats)
 
 y_train = df['Biopsy']
-
-
+y_test = X_test['Biopsy']
+X_test = X_test.drop('Biopsy', axis = 1)
 
 # # now let's put the data through a pipeline
 
@@ -186,23 +219,32 @@ full_pipeline = FeatureUnion(transformer_list=[
         ("num_pipeline", num_pipeline),
         ("cat_pipeline", cat_pipeline),
     ])
-# -
 
+# +
 X_train_processed = full_pipeline.fit_transform(X_train)
 X_train_processed = pd.DataFrame(X_train_processed)
 
+# note that we are using transform() on X_pretest 
+#we standardize our training dataset, we need to keep the parameters (mean and standard deviation for each feature). 
+#Then, we use these parameters to transform our test data and any future data later on
+#fit() just calculates the parameters (e.g. 𝜇 and 𝜎 in case of StandardScaler) 
+#and saves them as an internal objects state. 
+#Afterwards, you can call its transform() method to apply the transformation to a particular set of examples.
+X_test_processed = full_pipeline.transform(X_test)
+X_test_processed = pd.DataFrame(X_test_processed)
 
-X_train_processed
 
+
+# -
 
 
 X_train.to_csv(r'X_train.csv')
+X_test.to_csv(r'X_test.csv')
 y_train.to_csv(r'y_train.csv')
+y_test.to_csv(r'y_test.csv')
+
 
 X_train_processed.to_csv(r'X_train_processed.csv')
-
-y_train.shape
-
-y_train.to_csv(r'y_train2.csv')
+X_test_processed.to_csv(r'X_test_processed.csv')
 
 
